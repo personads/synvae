96e7c6d2cb453625580ca66f85b71775524b605a
Maximilian Müller-Eberstein <work@personads.me>
2019-06-18 12:47:53 +0200

    switched textual model to RNN

--- models/textual.py
+++ models/textual.py
@@ -19,8 +19,9 @@ class TextualVae(BaseModel):
         self.epoch = 0
         self._recon_loss_name = 'SeqXE'
         # init placeholders
-        self.originals = tf.placeholder(tf.int64, [self.batch_size, self.max_length], name='texts')
-        self.decoder_inputs = self.originals[:, :-1] # no end token
+        self.originals = tf.placeholder(tf.int32, [self.batch_size, self.max_length], name='texts')
+        self.embeddings = tf.Variable(tf.random_uniform([self.vocab_size, self.latent_dim]), name='embeddings')
+        # self.decoder_inputs = self.originals[:, :-1] # no end token
         self.tf_epoch = tf.placeholder(tf.int32, [], name='epoch')
 
 
@@ -53,30 +54,15 @@ class TextualVae(BaseModel):
 
     def calc_loss(self, originals, recon_logits, means, sigmas):
         # Reconstruction loss over sequence
-        originals = originals[:, 1:] # no start token
         padding_mask = tf.cast(tf.math.logical_not(tf.math.equal(originals, 0)), dtype=tf.float32)
         self.recon_loss = tf.reduce_sum(tf.contrib.seq2seq.sequence_loss(logits=recon_logits, targets=originals, weights=padding_mask, average_across_timesteps=False))
+
         # KL divergence
         self.latent_loss = tf.reduce_mean(-0.5 * tf.reduce_sum(1. + sigmas - tf.square(means) - tf.exp(sigmas), axis=-1)) # mean KL over latent dims
         beta_weight = tf.sigmoid(tf.cast(self.tf_epoch, dtype=tf.float32) - self.beta_half_life)
         self.bw = beta_weight
         loss = self.recon_loss + self.beta * beta_weight * self.latent_loss
         return loss
-
-
-    def build_embedding(self, inputs):
-        # reuse embedding layer since encoder and decoder "language" are one and the same
-        with tf.variable_scope('embedding', reuse=True):
-            input_embeddings = tf.keras.layers.Embedding(self.vocab_size, self.latent_dim)(inputs)
-
-        positional_encoded = transformer.positional_encoding(self.latent_dim, self.max_length-1)
-        position_inputs = tf.tile(tf.range(0, self.max_length-1), [self.batch_size])
-        position_inputs = tf.reshape(position_inputs, [self.batch_size, self.max_length-1]) # batch_size x [0, 1, 2, ..., n]
-
-        positional_embeddings = tf.add(input_embeddings, tf.nn.embedding_lookup(positional_encoded, position_inputs))
-        positional_embeddings = tf.nn.dropout(positional_embeddings, keep_prob=.8)
-
-        return positional_embeddings
 
 
     def build_encoder(self, texts):
@@ -88,8 +74,8 @@ class TextualVae(BaseModel):
 
 
     def run_train_step(self, tf_session, batch):
-        _, loss, recon_loss, latent_loss, means, sigmas, bw = tf_session.run([self.train_op, self.loss, self.recon_loss, self.latent_loss, self.means, self.sigmas, self.bw], feed_dict={self.originals: batch, self.tf_epoch: self.epoch})
-        losses = {'All': loss, self._recon_loss_name: recon_loss, 'KL': latent_loss, 'm': np.mean(means), 's': np.mean(sigmas), 'bw': bw}
+        _, loss, recon_loss, latent_loss, means, sigmas = tf_session.run([self.train_op, self.loss, self.recon_loss, self.latent_loss, self.means, self.sigmas], feed_dict={self.originals: batch, self.tf_epoch: self.epoch})
+        losses = {'All': loss, self._recon_loss_name: recon_loss, 'KL': latent_loss, 'm': np.mean(means), 's': np.mean(sigmas)}
         return losses, None
 
 
@@ -108,7 +94,7 @@ class TextualVae(BaseModel):
         recon_txt = self.convert_logits_to_texts(reconstructions[0:1])[0]
         export_text = '%d (%d): "%s"' % (epoch_idx, len(recon_txt), ' '.join(recon_txt))
         if epoch_idx == 1:
-            orig_txt = self.convert_indices_to_texts(originals[0:1, 1:])[0]
+            orig_txt = self.convert_indices_to_texts(originals[0:1])[0]
             export_text = 'ORIG (%d): "%s"\n' % (len(orig_txt), ' '.join(orig_txt)) + export_text
 
         self.save_text(export_text, os.path.join(out_path, str(batch_idx) + '_recons.txt'))
@@ -129,6 +115,8 @@ class TextualVae(BaseModel):
         for o in range(indices.shape[0]):
             cur_text = []
             for t in range(indices.shape[1]):
+                if indices[o, t] == self.vocab_size-2:
+                    continue
                 if indices[o, t] == self.vocab_size-1:
                     break
                 cur_text.append(self.idx_tkn_map[indices[o, t]])
@@ -142,44 +130,44 @@ class SarcVae(TextualVae):
 
 
     def build_encoder(self, texts):
-        encoder = transformer.Encoder(
-                 num_layers=4,
-                 num_heads=4,
-                 key_dim=32,
-                 value_dim=32,
-                 latent_dim=self.latent_dim,
-                 ffn_dim=32)
+        encoder_embeddings = tf.nn.embedding_lookup(self.embeddings, texts)
 
-        encoder_inputs = texts[:, 1:] # no start token
-        encoder_embeddings = self.build_embedding(encoder_inputs)
-        encoder_outputs = encoder.build(encoder_embeddings)
+        fw_cell = tf.contrib.rnn.GRUCell(num_units=1024)
+        encoder_outputs, encoder_states = tf.nn.dynamic_rnn(fw_cell, encoder_embeddings, dtype=tf.float32)
 
         # build VAE components with reparametrization
-        means = tf.keras.layers.Dense(self.latent_dim)(encoder_outputs)
-        sigmas = tf.keras.layers.Dense(self.latent_dim)(encoder_outputs)
-        epsilons = tf.random.normal([self.batch_size, self.max_length-1, self.latent_dim], mean=0., stddev=1.)
+        means = tf.keras.layers.Dense(self.latent_dim)(encoder_states)
+        sigmas = tf.keras.layers.Dense(self.latent_dim)(encoder_states)
+        epsilons = tf.random.normal([self.batch_size, self.latent_dim], mean=0., stddev=1.)
         latents = means + tf.exp(.5 * sigmas) * epsilons # (batch_size, max_length-1, latent_dim)
-
-        # reduce latents over sentence to mean (necessary for MusicVAE compatibility
-        latents = tf.reduce_mean(latents, axis=1) # (batch_size, latent_dim)
 
         return latents, means, sigmas
 
 
     def build_decoder(self, latents):
-        with tf.variable_scope('decoder', reuse=tf.AUTO_REUSE):
-            decoder = transformer.Decoder(
-                     num_layers=4,
-                     num_heads=4,
-                     key_dim=32,
-                     value_dim=32,
-                     latent_dim=self.latent_dim,
-                     ffn_dim=32)
+        cell = tf.contrib.rnn.GRUCell(num_units=self.latent_dim)
 
-            decoder_embeddings = self.build_embedding(self.decoder_inputs) # enforce information bottleneck
-            # replicate latent vector over sentence
-            latents = tf.tile(latents, [1, self.max_length-1]) # (batch_size, latent_dim * (max_length-1))
-            latents = tf.reshape(latents, [-1, self.max_length-1, self.latent_dim]) # (batch_size, max_length, latent_dim)
-            decoder_outputs = decoder.build(decoder_embeddings, latents)
-            recon_logits = tf.layers.dense(decoder_outputs, self.vocab_size) # (batch_size, max_length-1, vocab_size)
+        start_tokens = tf.fill([self.batch_size], self.vocab_size - 2)
+        end_token = self.vocab_size - 1
+        helper = tf.contrib.seq2seq.GreedyEmbeddingHelper(self.embeddings, start_tokens=start_tokens, end_token=end_token)
+
+        decoder = tf.contrib.seq2seq.BasicDecoder(
+                                            cell=cell,
+                                            helper=helper,
+                                            initial_state=latents,
+                                            output_layer=tf.layers.Dense(self.vocab_size))
+        outputs, states, lengths = tf.contrib.seq2seq.dynamic_decode(
+                                                        decoder=decoder,
+                                                        output_time_major=False,
+                                                        impute_finished=True,
+                                                        maximum_iterations=self.max_length)
+
+        recon_logits = tf.pad(
+                        outputs.rnn_output,
+                        [[0, 0], # no padding on batch
+                         [0, tf.maximum(self.max_length - tf.reduce_max(lengths), 0)], # pad missing length
+                         [0, 0]], # no padding on vocabulary
+                        constant_values=0,
+                        mode='CONSTANT')
+
         return recon_logits
